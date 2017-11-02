769ce04bd7324b70e94269c24cd73776a6493212
perekk <davidegessi@gmail.com>
2017-11-02 17:54:45 +0100

    scripts

---
+++ script.js
@@ -0,0 +1,6 @@
+var canvas = document.getElementById("canvas");
+var context = canvas.getContext("2d");
+for (var i = 0; i < 10000; i++) {
+}
+context.fillStyle = "red";
+context.fillRect(0, 0, 100, 100);
---
+++ script.ts
@@ -0,0 +1,8 @@
+let canvas = document.getElementById("canvas") as HTMLCanvasElement;
+let context: CanvasRenderingContext2D = canvas.getContext("2d");
+
+for(let i=0;i<10000;i++) {
+    
+}
+context.fillStyle = "red";
+context.fillRect(0,0,100,100);
